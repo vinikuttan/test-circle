6ac6f70af51b8f5c70df59c550ffdbe08466826a
Vineesh Kumar <vinesh.emag@gmail.com>
2022-07-27 11:28:08 +0530

    Update tests.py

--- pages/tests.py
+++ pages/tests.py
@@ -1,8 +1,38 @@
 import unittest
+import time
 
 class SimpleTest(unittest.TestCase):
     def test(self):
         self.assertTrue(True)
+        
+    def test_1(self):
+        time.sleep(3)
+        self.assertTrue(True)
+        
+    def test_2(self):
+        time.sleep(3)
+        self.assertTrue(True)   
+        
+    def test_3(self):
+        time.sleep(3)
+        self.assertTrue(True)
+        
+    def test_4(self):
+        time.sleep(3)
+        self.assertTrue(True)
+        
+    def test_5(self):
+        time.sleep(3)
+        self.assertTrue(True)
+        
+    def test_6(self):
+        time.sleep(3)
+        self.assertTrue(True)
+        
+        
+ 
+        
+        
 
 if __name__ == '__main__':
     unittest.main()
